844c48cd10b20a6823d32ce25474f0704e3738ba
Rukayat Olapojoye <olapojoyetemitope@gmail.com>
2021-06-28 17:10:07 +0100

    New !!! Version with user authentication (login and register) using Local storage.

--- src/components/stateprovider.jsx
+++ src/components/stateprovider.jsx
@@ -5,18 +5,37 @@ export const AppContext = createContext();
 const initialState = {
 
     // initial state stuffs here
+    isLoggedIn: false,
+    userId: null,
+    userEmail: null,
 };
 
 function reducer(state, action) {
 
+    let saveState = { ...state };
+
+
+    if (action.type === 'REGISTER') {
+        saveState.isLoggedIn = false;
+        saveState.userEmail = action.payload.userEmail;
+        saveState.userId = action.payload.userId;
+
+    }
+    if (action.type === 'LOGIN') {
+        saveState.isLoggedIn = true;
+        saveState.userEmail = action.payload.userEmail;
+        saveState.userId = action.payload.userId;
+
+    }
 
     if (action.type === 'LOGOUT') {
+        saveState.isLoggedIn = false;
+        saveState.userEmail = null;
+        saveState.userId = null;
+    }
+    return saveState;
 
-
-    };
 }
-
-
 
 
 export default function AppStates({ children }) {
