86f22d99857d7f5019b76f68d0527822c9e1f6ed
Dhanidayo <sarahjospeh@ymail.com>
2021-07-01 21:39:32 +0100

    fixed homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,5 @@
 import DefaultLayout from '../../layouts/default-layout';
-<<<<<<< HEAD
 import clipart from '../Home/clip-art.jpg';
-=======
-//import Mentorship3 from '../Home/Mentorship3.png';
->>>>>>> 01c68bb562f29e7aff393d7ad61622eda500e216
 
 // scoped style for this file
 import './home.css';
@@ -11,14 +7,11 @@ import './home.css';
 function Home() {
 	return (
 		<DefaultLayout>
-<<<<<<< HEAD
 			<div className="homepage">
 				<img src={clipart} alt="mentorship Pic" />
-=======
 			<div className="home-container">
->>>>>>> 01c68bb562f29e7aff393d7ad61622eda500e216
 				<h2>Welcome to Find-A-Mentor App</h2>
-
+			</div>
 			</div>
 		</DefaultLayout>
 	);
